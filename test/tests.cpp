456b7cf5306e34d088a1f9d302353b8d6a02544c
Hand-less <70288287+Hand-less@users.noreply.github.com>
2025-02-27 14:35:39 +0300

    Update tests.cpp
    Added test for numeric funtions

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,17 +1,61 @@
 // Copyright 2025 UNN-CS
 
 #include <gtest/gtest.h>
-#include <cstdint>
 #include "alg.h"
+#include <cstdint>
 
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(CheckPrimeTest, HandlesOne) {
+    EXPECT_FALSE(checkPrime(1));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(CheckPrimeTest, HandlesTwo) {
+    EXPECT_TRUE(checkPrime(2));
+}
+
+TEST(CheckPrimeTest, HandlesFour) {
+    EXPECT_FALSE(checkPrime(4));
+}
+
+TEST(CheckPrimeTest, HandlesLargePrime) {
+    EXPECT_TRUE(checkPrime(7919));
+}
+
+TEST(CheckPrimeTest, HandlesLargeNonPrime) {
+    EXPECT_FALSE(checkPrime(8000));
+}
+
+
+TEST(NPrimeTest, FirstPrime) {
+    EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(NPrimeTest, FifthPrime) {
+    EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(NPrimeTest, TenthPrime) {
+    EXPECT_EQ(nPrime(10), 29);
+}
+
+
+TEST(NextPrimeTest, AfterFour) {
+    EXPECT_EQ(nextPrime(4), 5);
+}
+
+TEST(NextPrimeTest, AfterEleven) {
+    EXPECT_EQ(nextPrime(11), 13);
+}
+
+
+TEST(SumPrimeTest, SumPrimesBelowTen) {
+    EXPECT_EQ(sumPrime(10), 17);  // 2 + 3 + 5 + 7 = 17
+}
+
+TEST(SumPrimeTest, SumPrimesBelowTwenty) {
+    EXPECT_EQ(sumPrime(20), 77);  // 2 + 3 + 5 + 7 + 11 + 13 + 17 + 19 = 77
+}
+
+TEST(SumPrimeTest, SumPrimesBelowOneHundred) {
+    EXPECT_EQ(sumPrime(100), 1060);
 }
